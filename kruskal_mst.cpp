42331de9f3f9a7fd53624573f9ee0d21cca6d02c
chaitak-gorai <chaitakgorai@gmail.com>
2023-07-08 06:46:52 +0530

    🚀 Day 28

---
+++ kruskal_mst.cpp
@@ -0,0 +1,102 @@
+class DisjointSet
+{
+    vector<int> rank, parent, size;
+
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution
+{
+public:
+    // Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+        vector<pair<int, pair<int, int>>> edges;
+        for (int i = 0; i < V; i++)
+        {
+            for (auto it : adj[i])
+            {
+                int weight = it[1];
+
+                int adjNode = it[0];
+                int node = i;
+                edges.push_back({weight, {node, adjNode}});
+            }
+        }
+        sort(edges.begin(), edges.end());
+        // for(auto it:edges){
+        //     cout<<it.first<<it.second.first<<it.second.second<<endl;
+        // }
+        DisjointSet ds(V);
+        int minPath = 0;
+        for (auto it : edges)
+        {
+            int u = it.second.first;
+            int v = it.second.second;
+            int wt = it.first;
+
+            if (ds.findUPar(u) != ds.findUPar(v))
+            {
+                minPath += wt;
+                ds.unionBySize(u, v);
+            }
+        }
+        return minPath;
+    }
